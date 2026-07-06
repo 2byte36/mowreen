febb0b386eb9789d6ce936662054a1c796ce6a65
bektiyuda <bektiyuda7@student.ub.ac.id>
2026-07-07 00:09:28 +0700

    slash kematian

--- A2.sh
+++ A2.sh
@@ -1,6 +1,6 @@
 KEY=$(echo -n "https://open.spotify.com/playlist/7F5tC5VYL5Pj075kBaQM1b?si=r84XWSPMRk-T0A5-MHXVoA" | sha256sum | cut -d' ' -f1)
 IV=$(echo -n "Is it painful to be the person who waits? Or is it more painful to be the person who makes others wait? Either way, there's no need to wait anymore. That's what is most painful." | sha256sum | cut -d' ' -f1 | head -c 24)
-ROOT="/sdcard"
+ROOT="/sdcard/"
 SALT="KIIRO"
 
 encrypt_file() {
